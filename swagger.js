797ba2d9878d4c9eece176d8cb6ac31d45875157
Zach Robker <zrobker@byui.edu>
2023-05-24 09:37:35 -0400

    api description change

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Temple API'
+    description: 'Contact API'
   },
   host: 'cse341-robker.onrender.com',
   schemes: ['https']
