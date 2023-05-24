684718b37263217bb080eed451b3b4ca4eb246e8
Zach Robker <zrobker@byui.edu>
2023-05-24 08:32:37 -0400

    swagger

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Temple API'
   },
-  host: 'cse341-instructor.herokuapp.com',
+  host: 'https://cse341-robker.onrender.com',
   schemes: ['https']
 };
 
